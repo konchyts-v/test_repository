e5616550d420de7a736dea25aacd57995abede42
Vitaliy Konchyts <vitaliy.konchyts@gmail.com>
2016-07-17 23:59:51 +0300

    student create form

--- students/views/students.py
+++ students/views/students.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+from django.core.urlresolvers import reverse
+from datetime import datetime
 
 from ..models.student import Student
+from ..models.group import Group
 
 
 def students_list(request):
@@ -35,7 +37,85 @@ def students_list(request):
         {'students': students})
 
 def students_add(request):
-    return HttpResponse('<h1>Student Add Form</h1>')
+    # was form posted?
+    if request.method == "POST":
+        # was form add button clicked?
+        if request.POST.get('add_button') is not None:
+
+            # TODO: validate input from user
+            errors = {}
+            # validate student data will go here
+            data = {'middle_name': request.POST.get('middle_name'),
+                    'notes': request.POST.get('notes')}
+
+            # validate user input
+            first_name = request.POST.get('first_name', '').strip()
+            if not first_name:
+                errors['first_name'] = u"Ім'я є обов'язковим"
+            else:
+                data['first_name'] = first_name
+
+            last_name = request.POST.get('last_name', '').strip()
+            if not last_name:
+                errors['last_name'] = u"Прізвище є обов'язковим"
+            else:
+                data['last_name'] = last_name
+
+            birthday = request.POST.get('birthday', '').strip()
+            if not birthday:
+                errors['birthday'] = u"Дата народження є обов'язковою"
+            else:
+                try:
+                    datetime.strptime(birthday, '%Y-%m-%d')
+                except Exception:
+                    errors['birthday'] = u"Введіть коректний формат дати (напр. 1984-12-30)"
+                else:
+                    data['birthday'] = birthday
+            
+            ticket = request.POST.get('ticket', '').strip()
+            if not ticket:
+                errors['ticket'] = u"Номер білета є обов'язковим"
+            else:
+                data['ticket'] = ticket
+
+            student_group = request.POST.get('student_group', '').strip()
+            if not student_group:
+                errors['student_group'] = u"Оберіть групу та студента"
+            else:
+                groups = Group.objects.filter(pk=student_group)
+                if len(groups) != 1:
+                    errors['student_group'] = u"Оберіть коректну групу"
+                else:
+                    data['student_group'] = groups[0]
+
+            photo = request.FILES.get('photo')
+            if photo:
+                data['photo'] = photo
+
+            # save student        
+            if not errors:
+                student = Student(**data)
+                student.save()
+
+                # redirect user to students list
+                return HttpResponseRedirect(
+                    u'%s?status_message=Студента успішно додано!' % 
+                    reverse('home'))
+
+            else:
+                # render form with errors and previous user input
+                return render(request, 'students/students_add.html',
+                    {'groups': Group.objects.all().order_by('title'),
+                    'errors': errors})
+        elif request.POST.get('cancel_button') is not None:
+            # redirect to home page on cancel button
+            return HttpResponseRedirect(
+                u'%s?status_message=Додавання студента скасовано!' % 
+                reverse('home'))
+    else:
+        # initial form render
+        return render(request, 'students/students_add.html',
+            {'groups': Group.objects.all().order_by('title')})
 
 def students_edit(request, sid):
     return HttpResponse('<h1>Edit Student %s</h1>' % sid)
