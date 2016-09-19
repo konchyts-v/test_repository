25d5ec79e5bf0b10357e560c54326c336dcae371
Vitaliy Konchyts <vitaliy.konchyts@gmail.com>
2016-09-19 12:29:38 +0300

    test util.py (get_current_group)

--- db.py
+++ db.py
@@ -3,8 +3,14 @@ DATABASES_for_pr = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': 'localhost',
-        'USER': 'students_user1',
-        'PASSWORD': '5282',
+        #'USER': 'students_user1',
+        #'PASSWORD': '5282',
+        'USER': 'root',
+        'PASSWORD': '01011997',
         'NAME': 'students_db1',
+        'TEST': {
+        	'CHARSET': 'utf8',
+        	'COLLATION': 'utf8_general_ci',
+        }
     }
 }
--- studentsdb/db.py
+++ studentsdb/db.py
@@ -3,8 +3,14 @@ DATABASES_for_pr = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': 'localhost',
-        'USER': 'students_user1',
+        #'USER': 'students_user1',
+        #'PASSWORD': '5282',
+        'USER': 'root',
         'PASSWORD': '5282',
         'NAME': 'students_db1',
+        'TEST': {
+        	'CHARSET': 'utf8',
+        	'COLLATION': 'utf8_general_ci',
+        },
     }
 }
