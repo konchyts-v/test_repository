06d739f2777ba1f8086f7b7918c404084122a9b1
Vitaliy Konchyts <vitaliy.konchyts@gmail.com>
2016-10-06 16:10:28 +0300

    created full test for group form page and start to create test for journal

--- students/tests/test_group_forms.py
+++ students/tests/test_group_forms.py
@@ -2,6 +2,7 @@ from django.test import TestCase, Client, override_settings
 from django.core.urlresolvers import reverse
 
 from students.models.group import Group
+from students.models.student import Student
 
 @override_settings(LANGUAGE_CODE='en')
 class TestGroupUpdateForm(TestCase):
@@ -28,3 +29,39 @@ class TestGroupUpdateForm(TestCase):
         self.assertIn("Leader", response.content)
         self.assertIn('name="save_button"', response.content)
         self.assertIn('name="cancel_button"', response.content)
+
+
+    def test_access(self):
+        # try to access form as anonymous user
+        response = self.client.get(self.url, follow=True)
+
+        # we have to get 200 status code and login page
+        self.assertEqual(response.status_code, 200)
+
+        # check if we're on login form
+        self.assertIn("Login", response.content)
+
+        # check redirect url
+        self.assertEqual(response.redirect_chain[0],
+                         ('/users/login/?next=/groups/1/edit/', 302))
+
+
+    def test_success(self):
+        # login as admin to access group form edit
+        self.client.login(username="admin", password="admin")
+
+        # get form
+        response = self.client.post(self.url, {"title": "Group3332", "leader": 2, "notes": "Hello world"}, follow=True)
+
+        # check if status code equal 200
+        self.assertEqual(response.status_code, 200)
+
+        # test updated group details
+        group = Group.objects.get(pk=1)
+        student = Student.objects.get(pk=2)
+        self.assertEqual(group.title, "Group3332")
+        self.assertEqual(unicode(group), u"%s (%s %s)" % (group.title, group.leader.first_name, group.leader.last_name))
+        self.assertEqual(group.notes, "Hello world")
+
+
+
---
+++ students/tests/test_journal.py
@@ -0,0 +1,8 @@
+from datetime import datetime, date
+
+from django import TestCase, Client
+
+from students.models.student import Student
+from students.models.group import Group
+from students.models.monthjournal import MonthJournal
+
